c34741fc93cb6a270d3a5ca54f1e8d0c2be9ba86
Amir JCS <amirmahdyjdx@gmail.com>
2024-10-14 00:22:03 +0330

    test: add TestStartPointSafe unit test item

--- Tests/MineSweeper_test.go
+++ Tests/MineSweeper_test.go
@@ -4,32 +4,60 @@ import (
 	"testing"
 
 	"github.com/AmirMahdyJebreily/MinesweeperGO/GameCore"
+	core "github.com/AmirMahdyJebreily/MinesweeperGO/GameCore"
 )
 
-var board = GameCore.MineSweeper{
-	BombsCount: 11,
-	Size:       GameCore.Cpl(8, 10),
-	Bombs: []GameCore.TCpl{
-		{0, 9}, {2, 0},
-		{7, 7}, {2, 9},
-		{2, 4}, {3, 8},
-		{0, 5}, {6, 4},
-		{4, 6}, {7, 1},
-		{6, 8}},
-}
+var board = core.Init(core.Cpl(8, 10), core.Cpl(5, 6), 11, []GameCore.TCpl{
+	{0, 9}, {2, 0},
+	{7, 7}, {2, 9},
+	{2, 4}, {3, 8},
+	{0, 5}, {6, 4},
+	{4, 6}, {7, 1},
+	{6, 8}})
 
 func TestPointNumber(t *testing.T) {
 	cases := []struct {
-		in   GameCore.MineSweeper
+		in   *core.MineSweeper
 		want int
 	}{ // arr elem's
-		{in: board, want: 2}, // index 0
+		{in: board, want: 2}, // first case
 	}
 
 	for _, c := range cases {
-		got := c.in.NumberOfPoint(GameCore.Cpl(3, 9))
+		got := c.in.NumberOfPoint(core.Cpl(3, 9))
 		if got != c.want {
 			t.Errorf("err, got : %v | want : %v", got, c.want)
+		}
+	}
+}
+
+func TestStartPointSafe(t *testing.T) {
+	// in this case the c.want should be 8
+	// and number of bombs should be maximum value which is this : "(size.I × size.J) - 1"
+
+	cases := []struct {
+		in struct {
+			sizeI, sizeJ int
+			start        core.TCpl
+		}
+		want int
+	}{
+		{struct {
+			sizeI, sizeJ int
+			start        core.TCpl
+		}{
+			sizeI: 3, sizeJ: 3, start: core.Cpl(1, 1),
+		}, 8}, // first case
+	}
+
+	for _, c := range cases {
+		for i := 0; i < 100; i++ {
+			randBoard := core.InitRand(core.Cpl(c.in.sizeI, c.in.sizeJ), c.in.start, (c.in.sizeI*c.in.sizeJ)-1)
+			got := randBoard.NumberOfPoint(c.in.start)
+			if got != c.want {
+				t.Errorf("err, got : %v | want : %v", got, c.want)
+				break
+			}
 		}
 	}
 }

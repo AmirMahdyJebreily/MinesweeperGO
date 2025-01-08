d333d74995bbaff0d08b46228702bb58b8a5ebc8
Amir JCS <amirmahdyjdx@gmail.com>
2025-01-08 23:53:28 +0330

    fix: bombs cells score numbers bugs

--- pkg/minesweeperlib/minesweeperlib.go
+++ pkg/minesweeperlib/minesweeperlib.go
@@ -9,17 +9,22 @@ func GetCellNumbers(rows, cols int, bombs map[int]int) [][]int {
 	}
 
 	for x, y := range bombs {
+		board[x][y] = -1
 		// add one score to 8 cells around bombs
 		for i := -1; i <= 1; i++ {
 			for j := -1; j <= 1; j++ {
 				u, v := x+i, y+j
 				if (u < cols && u >= 0) && (v < rows && v >= 0) {
-					
+
+					// prevent change the score of bombs cell
+					if board[u][v] == -1 {
+						continue
+					}
 					board[u][v]++
 				}
 			}
 		}
-
 	}
+
 	return board
 }

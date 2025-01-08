69b6c256745f2e72ca85ae91a895d198e5431af4
Amir JCS <amirmahdyjdx@gmail.com>
2025-01-08 23:52:00 +0330

    test: complete get number of cells tests

--- pkg/minesweeperlib/minesweeperlib_test.go
+++ pkg/minesweeperlib/minesweeperlib_test.go
@@ -12,10 +12,10 @@ func TestGetCellNumbers(t *testing.T) {
 		want       [][]int
 	}
 	cases := []CellNumbersTestCase{
-		{3, 3, map[int]int{1: 1}, [][]int{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}}},
-		{3, 3, map[int]int{1: 2}, [][]int{{0, 1, 1}, {0, 1, 0}, {0, 1, 1}}},
-		{3, 3, map[int]int{0: 2}, [][]int{{0, 1, 0}, {0, 1, 1}, {0, 0, 0}}},
-		{3, 3, map[int]int{0: 2, 1: 1}, [][]int{{1, 2, 0}, {1, 0, 2}, {1, 1, 1}}},
+		{3, 3, map[int]int{1: 1}, [][]int{{1, 1, 1}, {1, -1, 1}, {1, 1, 1}}},
+		{3, 3, map[int]int{1: 2}, [][]int{{0, 1, 1}, {0, 1, -1}, {0, 1, 1}}},
+		{3, 3, map[int]int{0: 2}, [][]int{{0, 1, -1}, {0, 1, 1}, {0, 0, 0}}},
+		{3, 3, map[int]int{0: 2, 1: 1}, [][]int{{1, 2, -1}, {1, -1, 2}, {1, 1, 1}}},
 	}
 	for _, c := range cases {
 		assert.Equal(t, c.want, GetCellNumbers(c.rows, c.cols, c.bombs))

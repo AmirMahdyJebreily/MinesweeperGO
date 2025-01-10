ee1a36abf59db941fcd9260496b6f76498461e17
Amir JCS <amirmahdyjdx@gmail.com>
2025-01-10 23:17:04 +0330

    feat: add keyboard controls to terminal game

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,18 +3,27 @@ package main
 import (
 	"fmt"
 	"github.com/AmirMahdyJebreily/MinesweeperGO/pkg/minesweeperlib"
+	"github.com/eiannone/keyboard"
+	"slices"
 	"strings"
 )
 
-const untaped = "∙"
+const unoppend = "∙"
 const bomb = "\u001b[41m\u001B[35mX\u001B[0m"
 
 // use \x1b[1;31m ansi escape code for colorise
 func coloriseNumber(n int) string {
-	return fmt.Sprintf("\x1b[1;3%vm%v\x1b[1;0m", n, n)
+	return fmt.Sprintf("\x1b[1;3%vm%v\x1b[0m", n, n)
 }
 
-func Sprintgridf(board *[][]int, bombs *[][2]int) *strings.Builder {
+func SprintCell(data string, selected bool) string {
+	if selected {
+		return fmt.Sprintf("\x1b[5m[%v]\u001B[25m  ", data)
+	}
+	return fmt.Sprintf(" %v   ", data)
+}
+
+func Sprintgridf(board *[][]int, bombs *[][2]int, oppend *[][2]int, selected [2]int) *strings.Builder {
 	rows, cols := len(*board), len((*board)[0])
 	var res strings.Builder
 	res.WriteString("\033[H\033[2J") // Clear the old screen before print new board
@@ -25,19 +34,30 @@ func Sprintgridf(board *[][]int, bombs *[][2]int) *strings.Builder {
 		res.WriteString(fmt.Sprintf(" %2d     ", i+1))
 	lines:
 		for j := 0; j < cols; j++ {
-			if (*board)[i][j] == -1 {
-				res.WriteString(fmt.Sprintf("%v    ", bomb))
+			isSelected := [2]int{j, i} == selected
+			if oppend == nil {
+				res.WriteString(SprintCell(unoppend, isSelected))
 				continue lines
 			}
 
-			res.WriteString(fmt.Sprintf("%v    ", coloriseNumber((*board)[i][j])))
+			if !slices.Contains(*oppend, [2]int{j, i}) {
+				res.WriteString(SprintCell(unoppend, isSelected))
+				continue lines
+			}
+
+			if (*board)[i][j] == -1 {
+				res.WriteString(SprintCell(bomb, isSelected))
+				continue lines
+			}
+
+			res.WriteString(SprintCell(coloriseNumber((*board)[i][j]), isSelected))
 
 		}
 		res.WriteString("\n")
 	}
 	res.WriteString(fmt.Sprintf("\n      "))
 	for j := 0; j < cols; j++ {
-		res.WriteString(fmt.Sprintf(" %2d  ", j+1))
+		res.WriteString(fmt.Sprintf("  %2d ", j+1))
 	}
 	res.WriteString(fmt.Sprintf("\n"))
 
@@ -45,6 +65,13 @@ func Sprintgridf(board *[][]int, bombs *[][2]int) *strings.Builder {
 }
 
 func main() {
+	if err := keyboard.Open(); err != nil {
+		panic(err)
+	}
+	defer func() {
+		_ = keyboard.Close()
+	}()
+
 	var cols, rows int
 	fmt.Println("Wellcome to CodeAgha's MineSweeper Game in terminal")
 	for {
@@ -68,22 +95,47 @@ func main() {
 	}
 
 	board := minesweeperlib.GetBoard(cols, rows)
-	fmt.Println((*Sprintgridf(board, nil)).String())
+	selected := [2]int{cols / 2, rows / 2}
+	fmt.Println((*Sprintgridf(board, nil, nil, selected)).String())
 	var x0, y0 int
-	for {
-		fmt.Print("Select a cell to start game: ")
-		_, scanError := fmt.Scanf("%v,%v", &x0, &y0)
-		if scanError != nil {
-			fmt.Println("Please Input values in format above: Columns,Rows")
-			continue
-		}
-		break
-	}
-	bombs := minesweeperlib.GetRandomBombs(cols, rows, x0-1, y0-1, bombsCount)
-	board = minesweeperlib.GetCellNumbers(board, bombs)
-	fmt.Println((*Sprintgridf(board, bombs)).String())
+	var bombs *[][2]int = nil
+	var oppend [][2]int = nil
 
-	//for {
-	//
-	//}
+	for {
+		char, key, err := keyboard.GetKey()
+		if err != nil {
+			panic(err)
+		}
+
+		if key == keyboard.KeyArrowLeft && selected[0] > 0 {
+			selected[0]--
+		}
+
+		if key == keyboard.KeyArrowRight && selected[0] < cols-1 {
+			selected[0]++
+		}
+
+		if key == keyboard.KeyArrowUp && selected[1] < rows-1 {
+			selected[1]++
+		}
+
+		if key == keyboard.KeyArrowDown && selected[1] > 0 {
+			selected[1]--
+		}
+
+		if (char == 'o' || char == 'O') && selected[0] < cols {
+			if bombs == nil {
+				x0, y0 = selected[0], selected[1]
+				oppend = make([][2]int, 0)
+				bombs = minesweeperlib.GetRandomBombs(cols, rows, x0-1, y0-1, bombsCount)
+				board = minesweeperlib.GetCellNumbers(board, bombs)
+			}
+			oppend = append((oppend), selected)
+		}
+
+		// update screen
+		fmt.Println((*Sprintgridf(board, bombs, &oppend, selected)).String())
+		fmt.Print("[Arrowkeys: move] [O: Open Cell] [F: Flag]")
+
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/AmirMahdyJebreily/MinesweeperGO
 
 go 1.23.0
 
-require github.com/stretchr/testify v1.10.0 // indirect
+require (
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+)

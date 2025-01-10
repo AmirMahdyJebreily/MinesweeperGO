feed3029c07ad3b94786ec1d1a48459abae965d4
Amir JCS <amirmahdyjdx@gmail.com>
2025-01-10 22:15:31 +0330

    feat: optimize and colorize the text

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,41 +3,45 @@ package main
 import (
 	"fmt"
 	"github.com/AmirMahdyJebreily/MinesweeperGO/pkg/minesweeperlib"
+	"strings"
 )
 
 const untaped = "∙"
 const bomb = "\u001b[41m\u001B[35mX\u001B[0m"
 
-func Sprintgridf(board *[][]int, bombs *[][2]int) string {
-	cols, rows := len(*board), len((*board)[0])
-	res := ""
-	for i := 0; i < cols; i++ {
-		if i == 0 {
-			res += "      "
-			for j := 0; j < rows; j++ {
-				res += fmt.Sprintf(" %02d  ", j+1)
-			}
-			res += fmt.Sprintf("\n\n")
+// use \x1b[1;31m ansi escape code for colorise
+func coloriseNumber(n int) string {
+	return fmt.Sprintf("\x1b[1;3%vm%v\x1b[1;0m", n, n)
+}
 
-		}
-		res += fmt.Sprintf(" %02d     ", i+1)
-	lines:
-		for j := 0; j < rows; j++ {
-			if bombs != nil {
-				thisPos := [2]int{i, j}
-				for _, bom := range *bombs {
-					if thisPos == bom {
-						res += fmt.Sprintf("%v    ", bomb)
-						continue lines
-					}
-				}
-			}
-			res += fmt.Sprintf("%v    ", (*board)[i][j])
-
-		}
-		res += "\n"
+func Sprintgridf(board *[][]int, bombs *[][2]int) *strings.Builder {
+	rows, cols := len(*board), len((*board)[0])
+	var res strings.Builder
+	res.WriteString("\033[H\033[2J") // Clear the old screen before print new board
+	if bombs != nil {
+		res.WriteString(fmt.Sprintf(" .:: [Size: %v×%v] [Bombs: %v]\n", cols, rows, len(*bombs)))
 	}
-	return res
+	for i := rows - 1; i >= 0; i-- {
+		res.WriteString(fmt.Sprintf(" %2d     ", i+1))
+	lines:
+		for j := 0; j < cols; j++ {
+			if (*board)[i][j] == -1 {
+				res.WriteString(fmt.Sprintf("%v    ", bomb))
+				continue lines
+			}
+
+			res.WriteString(fmt.Sprintf("%v    ", coloriseNumber((*board)[i][j])))
+
+		}
+		res.WriteString("\n")
+	}
+	res.WriteString(fmt.Sprintf("\n      "))
+	for j := 0; j < cols; j++ {
+		res.WriteString(fmt.Sprintf(" %2d  ", j+1))
+	}
+	res.WriteString(fmt.Sprintf("\n"))
+
+	return &res
 }
 
 func main() {
@@ -45,9 +49,9 @@ func main() {
 	fmt.Println("Wellcome to CodeAgha's MineSweeper Game in terminal")
 	for {
 		fmt.Print("Enter The Columns,Rows: ")
-		_, scanError := fmt.Scanf("%v,%v", &cols, &rows)
+		_, scanError := fmt.Scanf("%v,%v\n", &cols, &rows)
 		if scanError != nil {
-			fmt.Println("Please Input values in format above: Columns,Rows")
+			fmt.Println("Please Input values in format: Columns,Rows")
 			continue
 		}
 		break
@@ -55,19 +59,19 @@ func main() {
 	var bombsCount int
 	for {
 		fmt.Print("Enter The count of bombs: ")
-		_, scanError := fmt.Scanf("%v", &bombsCount)
+		_, scanError := fmt.Scanln(&bombsCount)
 		if scanError != nil {
-			fmt.Println("Please Input values in format above: Columns,Rows")
+			fmt.Println("Please Input a number")
 			continue
 		}
 		break
 	}
 
 	board := minesweeperlib.GetBoard(cols, rows)
-	fmt.Println(Sprintgridf(board, nil))
+	fmt.Println((*Sprintgridf(board, nil)).String())
 	var x0, y0 int
 	for {
-		fmt.Print("Select: ")
+		fmt.Print("Select a cell to start game: ")
 		_, scanError := fmt.Scanf("%v,%v", &x0, &y0)
 		if scanError != nil {
 			fmt.Println("Please Input values in format above: Columns,Rows")
@@ -75,9 +79,9 @@ func main() {
 		}
 		break
 	}
-	bombs := minesweeperlib.GetRandomBombs(cols, rows, x0, y0, bombsCount)
+	bombs := minesweeperlib.GetRandomBombs(cols, rows, x0-1, y0-1, bombsCount)
 	board = minesweeperlib.GetCellNumbers(board, bombs)
-	fmt.Println(Sprintgridf(board, bombs))
+	fmt.Println((*Sprintgridf(board, bombs)).String())
 
 	//for {
 	//
--- pkg/minesweeperlib/minesweeperlib.go
+++ pkg/minesweeperlib/minesweeperlib.go
@@ -21,7 +21,7 @@ func GetRandomBombs(cols, rows, x0, y0 int, count int) *[][2]int {
 	bombs := make([][2]int, 0)
 
 	// The radius of the circle that determines how many points around the point (x0, y0) are forbidden points.
-	const radiusFactor = 0.15            // TODO:Add the degree of hardness: the harder, the smaller the radius
+	const radiusFactor = 0.16            // TODO:Add the degree of hardness: the harder, the smaller the radius
 	var exclusionCenter = [2]int{x0, y0} // the point that user clicked on it for first (x0 , y0)
 
 	// The radius of the circle must be specified based on the width of the page.
@@ -64,19 +64,19 @@ func GetCellNumbers(board *[][]int, bombs *[][2]int) *[][]int {
 	cols := len((*board)[0])
 
 	for bi := range *bombs {
-		x, y := (*bombs)[bi][0], (*bombs)[bi][1]
-		(*board)[x][y] = -1
+		x0, y0 := (*bombs)[bi][0], (*bombs)[bi][1]
+		(*board)[y0][x0] = -1
+
 		// add one score to 8 cells around bombs
 		for i := -1; i <= 1; i++ {
 			for j := -1; j <= 1; j++ {
-				u, v := x+i, y+j
-				if (u < cols && u >= 0) && (v < rows && v >= 0) {
-
+				x, y := x0+i, y0+j
+				if (x < cols && x >= 0) && (y < rows && y >= 0) {
 					// prevent change the score of bombs cell
-					if (*board)[u][v] == -1 {
+					if (*board)[y][x] == -1 {
 						continue
 					}
-					(*board)[u][v]++
+					(*board)[y][x]++
 				}
 			}
 		}

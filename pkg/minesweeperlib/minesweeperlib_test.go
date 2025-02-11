321f28ebd72215f4e96831ab854fea714d80a4fd
Amir JCS <amirmahdyjdx@gmail.com>
2025-02-11 23:53:15 +0330

    test: match tests to latest type changes

--- pkg/minesweeperlib/minesweeperlib_test.go
+++ pkg/minesweeperlib/minesweeperlib_test.go
@@ -1,50 +1,50 @@
 package minesweeperlib_test
 
 import (
-	"github.com/AmirMahdyJebreily/MinesweeperGO/pkg/minesweeperlib"
+	mnsw "github.com/AmirMahdyJebreily/MinesweeperGO/pkg/minesweeperlib"
 	"github.com/stretchr/testify/assert"
 	"slices"
 	"testing"
 )
 
 func TestGetBoard(t *testing.T) {
-	cases := [3]([2]int){
+	cases := [3](mnsw.Point){
 		{3, 3},
 		{3, 6},
 		{6, 3},
 	}
 	for _, want := range cases {
-		res := *minesweeperlib.GetBoard(want[0], want[1])
-		actual := [2]int{len((res)[0]), len(res)}
+		res := *mnsw.GetBoard(want)
+		var actual mnsw.Point = mnsw.AsPoint(int8(len((res)[0])), int8(len(res)))
 		assert.Equal(t, want, actual)
 	}
 }
 
 func TestGetRandomBombs(t *testing.T) {
-	board := minesweeperlib.GetBoard(3, 3)
-	bombs := *minesweeperlib.GetRandomBombs(board, 1, 1, 8)
-	assert.False(t, slices.Contains(bombs, [2]int{1, 1}))
+	board := mnsw.GetBoard(mnsw.AsPoint(3, 3))
+	bombs := *mnsw.GetRandomBombs(board, mnsw.AsPoint(1, 1), 8)
+	assert.False(t, slices.Contains(bombs, mnsw.AsPoint(1, 1)))
 }
 
 func TestGetCellNumbers(t *testing.T) {
 	type CellNumbersTestCase struct {
-		rows, cols int
-		bombs      [][2]int
-		want       [][]int
+		rows, cols int8
+		bombs      mnsw.Points
+		want       mnsw.Boardframe
 	}
 	cases := [4]CellNumbersTestCase{
-		{3, 3, [][2]int{{1, 1}}, [][]int{{1, 1, 1}, {1, -1, 1}, {1, 1, 1}}},
-		{3, 3, [][2]int{{2, 1}}, [][]int{{0, 1, 1}, {0, 1, -1}, {0, 1, 1}}},
-		{3, 3, [][2]int{{2, 0}}, [][]int{{0, 1, -1}, {0, 1, 1}, {0, 0, 0}}},
-		{3, 3, [][2]int{{2, 0}, {1, 1}}, [][]int{{1, 2, -1}, {1, -1, 2}, {1, 1, 1}}},
+		{3, 3, [][2]int8{{1, 1}}, [][]int8{{1, 1, 1}, {1, -1, 1}, {1, 1, 1}}},
+		{3, 3, [][2]int8{{2, 1}}, [][]int8{{0, 1, 1}, {0, 1, -1}, {0, 1, 1}}},
+		{3, 3, [][2]int8{{2, 0}}, [][]int8{{0, 1, -1}, {0, 1, 1}, {0, 0, 0}}},
+		{3, 3, [][2]int8{{2, 0}, {1, 1}}, [][]int8{{1, 2, -1}, {1, -1, 2}, {1, 1, 1}}},
 	}
 	for _, c := range cases {
-		assert.Equal(t, &c.want, minesweeperlib.GetCellNumbers(minesweeperlib.GetBoard(c.cols, c.rows), &c.bombs))
+		assert.Equal(t, &c.want, mnsw.GetCellNumbers(mnsw.GetBoard(mnsw.AsPoint(c.cols, c.rows)), &c.bombs))
 	}
 
 }
 
 func TestGetOpeneds(t *testing.T) {
-	o := minesweeperlib.GetOpeneds(minesweeperlib.GetBoard(3, 3), 2, 2)
-	assert.Contains(t, o, [2]int{2, 2})
+	o := mnsw.GetOpeneds(mnsw.GetBoard(mnsw.AsPoint(3, 3)), mnsw.AsPoint(2, 2))
+	assert.Contains(t, o, [2]int8{2, 2})
 }
